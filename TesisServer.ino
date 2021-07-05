7a75e259d6d86fa732ac049bdd093a344db22edb
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-07-05 10:25:04 -0400

    Feat: Added Firebase
    BREAKING CHANGES
    
    AWS Deleted
    Firebase Added

--- TesisServer.ino
+++ TesisServer.ino
@@ -10,35 +10,72 @@
 #include <Wire.h>
 #include <ESP32Ping.h>
 #include <ping.h>
+#include <FirebaseESP32.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
  
 
-
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
 /****DEFINICIONES****/
-#define BUFFER_LEN  256
-#define AWS_IOT_PUBLISH_TOPIC   "data/sensors"
-#define AWS_IOT_SUBSCRIBE_TOPIC "data/recibo"
 #define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 
 /****VARIABLES****/
-char msg[BUFFER_LEN];
+// Firebase Data object
+FirebaseData firebaseData;
+FirebaseJson json;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// WifiManager Variables
 WiFiManager wifiManager;
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
+
+// Data Variables
 String packSize = "--";
 String packet;
+String path = "/Sensores";
 int pos1,pos2,pos3;
-String temp,spo2,bpm,ID;
+String temp,spo2,hr,ID,nodo;
+float tempF,spo2F,hrF;
+char tempC[8],spo2C[8],hrC[8];
 
-void messageHandler(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+// NTP Variables
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "south-america.pool.ntp.org");
+unsigned long epochTime; 
+
+// GET EPOCH
+unsigned long get_Time() {
+  timeClient.update();
+  unsigned long now = timeClient.getEpochTime();
+  return now;
+}
+
+
+void connectFirebase(){
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = FIREBASE_HOST;
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  Firebase.begin(&config, &auth);
+}
+
+
+void sendFirebase() {
+  epochTime = get_Time();
+  String nodo = path + "/"+ID+"/";
+  String nodo1 = "/Historicos/"+ID+"/"+String(epochTime)+"/";
+  json.add("spo2", spo2F);
+  json.add("hr", hrF);
+  json.add("temp", tempF);   
+  Firebase.updateNode(firebaseData,nodo,json);
+  Firebase.updateNode(firebaseData,nodo1,json);            
 }
 
 void logo(){
@@ -46,60 +83,28 @@ void logo(){
   Heltec.display->drawXbm(0,5,logo_width,logo_height,logo_bits);
   Heltec.display->display();
 }
-void connectAWS()
-{
-  // Configure WiFiClientSecure to use the AWS IoT device credentials
-  net.setCACert(AWS_CERT_CA);
-  net.setCertificate(AWS_CERT_CRT);
-  net.setPrivateKey(AWS_CERT_PRIVATE);
 
-  // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.setServer(AWS_IOT_ENDPOINT, 8883);
-
-  // Create a message handler
-  client.setCallback(messageHandler);
-
-  Serial.print("Connecting to AWS IOT");
-
-  while (!client.connect(THINGNAME)) {
-    Serial.print(".");
-    delay(100);
-  }
-
-  if(!client.connected()){
-
-    Heltec.display->drawString(0, 30, "Error con la red, reinicie el dispositivo y pruebe con otra red");
-    Serial.println("AWS IoT Timeout!");
-    return;
-  }
-
-  // Subscribe to a topic
-  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-
-  Serial.println("AWS IoT Connected!");
-}
-
-void publishMessage()
-{
-  Serial.print("ESTOY ENVIANDO POR AWS"); 
-  snprintf (msg, BUFFER_LEN,"{ \"ID\": \"%s\" , \"Temprature\" : %s , \"Pulsaciones\": %s , \"Oxigenación\": %s }",ID.c_str(),temp.c_str(),bpm.c_str(),spo2.c_str());
-  Serial.println(msg);
-  client.publish(AWS_IOT_PUBLISH_TOPIC,msg);
-  Serial.print("Mensaje enviado");
-}
 void LoRaData(){
   temp="";
-  bpm="";
+  hr="";
   spo2="";
   ID="";
   pos1=packet.indexOf("#");
   pos2=packet.indexOf("$");
   pos3=packet.indexOf("/");
   temp=packet.substring(0,pos1);
-  bpm=packet.substring(pos1+1,pos2);
+  hr=packet.substring(pos1+1,pos2);
   spo2=packet.substring(pos2+1,pos3);
   ID=packet.substring(pos3+1,packet.length());
- 
+
+
+  // Convertir String a Float
+  temp.toCharArray(tempC,temp.length()+1);
+  spo2.toCharArray(spo2C,spo2.length()+1);
+  hr.toCharArray(hrC,hr.length()+1);
+  tempF=atof(tempC);
+  spo2F=atof(spo2C);
+  spo2F=atof(hrC);
 }
 
 void cbk(int packetSize) {
@@ -133,7 +138,7 @@ void setup() {
     WiFi.disconnect();
     wifiManager.autoConnect("Covid-Monitor-Server");
   }
-  connectAWS();
+    connectFirebase();
   Heltec.display->clear();
   Heltec.display->drawString(0, 0, "LoRa Iniciado y Wi-fi Exitosamente!");
   Heltec.display->display();
@@ -153,7 +158,7 @@ void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) { 
     cbk(packetSize); 
-    publishMessage();
+    sendFirebase();
     }
   delay(10);
 }
